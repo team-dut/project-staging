006c79eba2734d2be7832417f10b47244181a25d
Tien Dat Pham <dat20036@gmail.com>
2023-04-27 20:12:27 +0700

    Add .exe file creation via GitHub CI ...
    ... and remove accidentally added Game.jar

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,8 @@ import UI.StartWindow;
 
 public class Main {
     public static void main(String[] args) {
+        System.err.printf("Current working directory: %s\n", System.getProperty("user.dir"));
+
         System.setProperty("sun.java2d.opengl", "True");
 
         new StartWindow();
