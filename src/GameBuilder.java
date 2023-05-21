22a60ae1c2d4681e87477611904492347b1feb5d
Tien Dat Pham <dat20036@gmail.com>
2023-05-21 10:59:22 +0700

    Rewrite the code to use builder pattern

---
+++ src/GameBuilder.java
@@ -0,0 +1,39 @@
+import ABC.BaseExtension;
+import UI.StartWindow;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class GameBuilder {
+    private final ArrayList<BaseExtension> extensions;
+
+    public GameBuilder() {
+        this.extensions = new ArrayList<>();
+    }
+
+    public ArrayList<BaseExtension> getExtensions() {
+        return extensions;
+    }
+
+    public GameBuilder addExtension(BaseExtension extension) {
+        getExtensions().add(extension);
+        return this;
+    }
+
+    public void start() {
+        System.setProperty("sun.java2d.opengl", "True");
+        System.setProperty("sun.java2d.xrender", "True");
+
+        for (BaseExtension ext: getExtensions()) {
+            new Thread(() -> {
+                try {
+                    ext.loadExtension();
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }).start();
+        }
+
+        new StartWindow();
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,11 @@
-import UI.StartWindow;
+import Extensions.DiscordExtension;
 
 public class Main {
     public static void main(String[] args) {
         System.err.printf("Current working directory: %s\n", System.getProperty("user.dir"));
 
-        System.setProperty("sun.java2d.opengl", "True");
-        System.setProperty("sun.java2d.xrender", "True");
-        new StartWindow();
+        new GameBuilder()
+                .addExtension(DiscordExtension.getExtension())
+                .start();
     }
 }
