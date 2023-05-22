d62864565813658b2c8710f1974e40bb404eaecf
Tien Dat Pham <dat20036@gmail.com>
2023-05-22 14:56:42 +0700

    Fix the case where the history is non-existent
    Signed-off-by: Tien Dat Pham <dat20036@gmail.com>

--- src/UI/PlayHistory.java
+++ src/UI/PlayHistory.java
@@ -47,40 +47,47 @@ public class PlayHistory extends JFrame {
 
         Path leaderboardFilePath = Paths.get("resources/history.txt");
 
+        record_add:
         try {
             String content = new String(Files.readAllBytes(leaderboardFilePath), StandardCharsets.UTF_8);
 
+            if (content.equals("")) break record_add;
+
             ArrayList<String> rankings = new ArrayList<>(Arrays.asList(content.split("\n")));
+
             Collections.reverse(rankings);
             List<String> history = rankings.subList(0, Math.min(10, rankings.size()));
 
-            for (String ranking: history) {
-                String[] elements = ranking.split(",");
+            if (history.size() >= 1)
+                for (String ranking : history) {
+                    String[] elements = ranking.split(",");
 
-                String date = elements[0];
-                String name = elements[1];
-                String score = elements[2];
-                String duration = elements[3];
-                String mapType = elements[4];
-                String gameMode = elements[5];
-                String gameResult = elements[6];
+                    if (Arrays.stream(elements).count() > 1) break;
 
-                JLabel label = new JLabel(String.format(
-                        "[%s] -- Name: %s -- Score %s -- Total time: %s -- Map: %s -- Mode: %s -- Result: %s",
-                        date,
-                        name,
-                        score,
-                        duration,
-                        mapType,
-                        gameMode,
-                        gameResult
-                ));
+                    String date = elements[0];
+                    String name = elements[1];
+                    String score = elements[2];
+                    String duration = elements[3];
+                    String mapType = elements[4];
+                    String gameMode = elements[5];
+                    String gameResult = elements[6];
 
-                label.setForeground(Color.yellow);
-                label.setFont(new Font(Font.MONOSPACED, Font.PLAIN, 13));
+                    JLabel label = new JLabel(String.format(
+                            "[%s] -- Name: %s -- Score %s -- Total time: %s -- Map: %s -- Mode: %s -- Result: %s",
+                            date,
+                            name,
+                            score,
+                            duration,
+                            mapType,
+                            gameMode,
+                            gameResult
+                    ));
 
-                historyPanel.add(label);
-            }
+                    label.setForeground(Color.yellow);
+                    label.setFont(new Font(Font.MONOSPACED, Font.PLAIN, 13));
+
+                    historyPanel.add(label);
+                }
         } catch (IOException ex) {
             throw new RuntimeException(ex);
         }
